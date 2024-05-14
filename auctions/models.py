9af283a0b472796c9e8d7e4f446a204de6e2a9d7
basuabhirup <basu.abhirup27@gmail.com>
2024-05-14 18:04:14 +0530

    Update DB Model to include wathcers in a Listing and Migrate DB accordingly

--- auctions/models.py
+++ auctions/models.py
@@ -14,6 +14,7 @@ class Listing(models.Model):
   active = models.BooleanField(default=True)
   creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
   current_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+  watchers = models.ManyToManyField(User, related_name="watchlist", blank=True)
 
   def __str__(self):
     return f"{self.title} ({self.creator.username})"
